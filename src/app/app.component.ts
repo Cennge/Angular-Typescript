1238d64fb24e6649a9ec38ace199d3467669d19c
Ilya <ilyagliba02@gmail.com>
2025-06-04 00:44:11 +0300

    added footer and discover container parts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { VacancyComponent } from './vacancy/vacancy.component';
+import { FooterComponent } from './footer/footer.component';
+import { DiscoverComponent } from './discover/discover.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, WelcomeComponent, VacancyComponent],
+  imports: [RouterOutlet, NavbarComponent, WelcomeComponent, VacancyComponent, FooterComponent, DiscoverComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/discover/discover.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonDescriptionComponent } from '../button-description/button-description.component';
+
+@Component({
+  selector: 'app-discover',
+  imports: [ButtonDescriptionComponent],
+  templateUrl: './discover.component.html',
+  styleUrl: './discover.component.css'
+})
+export class DiscoverComponent {
+
+}
